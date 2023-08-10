5b35b67ef6ed83aa9ba2845f8bb215c06a6614f1
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2023-08-10 19:17:31 +0200

    Update config-helpers.js

--- src/js/lib/config-helpers.js
+++ src/js/lib/config-helpers.js
@@ -1,10 +1,14 @@
 function getTileTypeId(config, type) {
-  const entry = Object.entries(config.tileTypes).find(([, props]) => props.type === type);
+  const entry = Object.entries(config.tileTypes).find(
+    ([, props]) => props.type === type
+  );
   return entry ? Number(entry[0]) : null;
 }
 
 function getTileType(config, type) {
-  const entry = Object.entries(config.tileTypes).find(([, props]) => props.type === type);
+  const entry = Object.entries(config.tileTypes).find(
+    ([, props]) => props.type === type
+  );
   return entry ? entry[1] : null;
 }
 
