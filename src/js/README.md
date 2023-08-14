9b2d5c0cb98249f7c9d141fc6e76905b8705f804
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2023-08-14 20:31:47 +0200

    added some more content

--- src/js/README.md
+++ src/js/README.md
@@ -17,14 +17,33 @@ The code in this directory is related to showing cars on the roads of a MapView.
 
 ## dashboard
 
-Most if this is not used in this project, since power up and the action elements are not longer used.
+Most of this is not used in this project, since power up and the action elements are not longer used.
 They are just a leftover from the future mobility parent project.
 
 ## data-sources
 
 Here the main calculations will be done. The postfix \_OLD refers to an unused file from the parent project. This is caused by changes done in the original file but to keep the old file, too, it got the postfix.
 
-File(s) with the postfix \_WT refer to files added for this project. WT stands for wind turbine. Currently only one file contains this postfix, that is the file for cacaulations with the data type wind turbine.
+File(s) with the postfix \_WT refer to files added for this project. WT stands for wind turbine. Currently only one file contains this postfix, that is the file for calculations with the data type wind turbine. Here it gets checked, of the amount of energy is less, more or equal to the required amound and if the distances between wind turbines and the other objects as well as other wind turbines will be kept.
+
+## editor
+
+Here are files from the original project, too. They are marked with the postfix \_OLD and are not used any longer. The contained files are for getting used by editor.html. The modal-....js files are used for exporting, importing, loading, saving and storing scenarios. Power ups is connected with the power-ups folder.
+
+## lib
+
+These are helper functions and classes from the original project.
+
+## power-ups
+
+Power ups are a leftover from the original project. They were use to manipulate the future mobility influencing aspects. In this project they are not used.
+
+## test
+
+scenarios.js is a file from the original project.
+cities.json contains two different example scenarios that can be loaded by an action button in the index.html file.
+
+## Remaining files in the js folder
 
 ## Miscellaneous
 
--- src/js/test/scenarios.js
+++ src/js/test/scenarios.js
@@ -1,28 +1,105 @@
 /* eslint-disable prefer-destructuring */
-const Car = require('../cars/car');
-const RoadTile = require('../cars/road-tile');
-const Cities = require('./cities.json');
+const Car = require("../cars/car");
+const RoadTile = require("../cars/road-tile");
+const Cities = require("./cities.json");
 
 function fiveCars(city, carOverlay) {
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['car-white'], 2, 0, 'N', RoadTile.INNER_LANE));
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['car-red'], 5, 0, 'N', RoadTile.OUTER_LANE));
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['car-blue'], 8, 0, 'N', RoadTile.INNER_LANE));
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['van-white'], 11, 0, 'N', RoadTile.OUTER_LANE));
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['bus-yellow'], 14, 0, 'N', RoadTile.INNER_LANE));
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["car-white"],
+      2,
+      0,
+      "N",
+      RoadTile.INNER_LANE
+    )
+  );
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["car-red"],
+      5,
+      0,
+      "N",
+      RoadTile.OUTER_LANE
+    )
+  );
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["car-blue"],
+      8,
+      0,
+      "N",
+      RoadTile.INNER_LANE
+    )
+  );
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["van-white"],
+      11,
+      0,
+      "N",
+      RoadTile.OUTER_LANE
+    )
+  );
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["bus-yellow"],
+      14,
+      0,
+      "N",
+      RoadTile.INNER_LANE
+    )
+  );
 }
 
 fiveCars.city = Cities.cities[0];
 
 function carInFront(city, carOverlay) {
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['car-white'], 8, 0, 'N', RoadTile.OUTER_LANE));
-  const obstacle = new Car(carOverlay, carOverlay.textures['car-red'], 8, 2, 'N', RoadTile.OUTER_LANE);
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["car-white"],
+      8,
+      0,
+      "N",
+      RoadTile.OUTER_LANE
+    )
+  );
+  const obstacle = new Car(
+    carOverlay,
+    carOverlay.textures["car-red"],
+    8,
+    2,
+    "N",
+    RoadTile.OUTER_LANE
+  );
   window.car = obstacle;
   obstacle.maxSpeed = 0;
   obstacle.speed = 0;
   carOverlay.addCar(obstacle);
 
-  carOverlay.addCar(new Car(carOverlay, carOverlay.textures['car-white'], 11, 0, 'N', RoadTile.OUTER_LANE));
-  const obstacle2 = new Car(carOverlay, carOverlay.textures['car-red'], 11, 2, 'N', RoadTile.OUTER_LANE);
+  carOverlay.addCar(
+    new Car(
+      carOverlay,
+      carOverlay.textures["car-white"],
+      11,
+      0,
+      "N",
+      RoadTile.OUTER_LANE
+    )
+  );
+  const obstacle2 = new Car(
+    carOverlay,
+    carOverlay.textures["car-red"],
+    11,
+    2,
+    "N",
+    RoadTile.OUTER_LANE
+  );
   obstacle2.onExitTile = () => {
     obstacle2.maxSpeed = 0;
     obstacle2.speed = 0;
@@ -33,9 +110,24 @@ function carInFront(city, carOverlay) {
 carInFront.city = Cities.cities[1];
 
 function trafficLight(city, carOverlay) {
-  const carNorth = new Car(carOverlay, carOverlay.textures['car-white'], 6, 4, 'N', RoadTile.OUTER_LANE);
+  const carNorth = new Car(
+    carOverlay,
+    carOverlay.textures["car-white"],
+    6,
+    4,
+    "N",
+    RoadTile.OUTER_LANE
+  );
   carOverlay.addCar(carNorth);
-  const carWest = new Car(carOverlay, carOverlay.textures['car-red'], 4, 6, 'W', RoadTile.OUTER_LANE, 0.85);
+  const carWest = new Car(
+    carOverlay,
+    carOverlay.textures["car-red"],
+    4,
+    6,
+    "W",
+    RoadTile.OUTER_LANE,
+    0.85
+  );
   carOverlay.addCar(carWest);
 }
 
@@ -43,9 +135,24 @@ trafficLight.city = Cities.cities[2];
 
 function trafficLightTimeout(city, carOverlay) {
   for (let i = 0; i !== 10; i += 1) {
-    const carNorth = new Car(carOverlay, carOverlay.textures['car-white'], 6, 4, 'N', RoadTile.OUTER_LANE);
+    const carNorth = new Car(
+      carOverlay,
+      carOverlay.textures["car-white"],
+      6,
+      4,
+      "N",
+      RoadTile.OUTER_LANE
+    );
     carOverlay.addCar(carNorth);
-    const carWest = new Car(carOverlay, carOverlay.textures['car-red'], 4, 6, 'W', RoadTile.OUTER_LANE, 0.85);
+    const carWest = new Car(
+      carOverlay,
+      carOverlay.textures["car-red"],
+      4,
+      6,
+      "W",
+      RoadTile.OUTER_LANE,
+      0.85
+    );
     carOverlay.addCar(carWest);
   }
 }
